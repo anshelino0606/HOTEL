4d801cbf9c8955c241765a8df6aeb481966d5b69
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-23 08:14:18 +0300

    main functionality testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+#include "Functionality/Hotel/Hotel.h"
+#include "Functionality/LinkedList/DoubleLinkedList.hpp"
+#include "Functionality/Guest/Guest.h"
+#include "Functionality/Rooms/Room.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //
+    DoubleLinkedList<Hotel> hotels;
+
+    Hotel hotel1("Rixos Luxury Resort&SPA");
+    Hotel hotel2("Steinberger Hotel");
+    Hotel hotel3("Hilton Hotel");
+
+    hotels.pushBack(hotel1);
+    hotels.pushBack(hotel2);
+    hotels.pushBack(hotel3);
+
+
+    Node<Hotel>* current = hotels.head;
+    while (current != nullptr) {
+        std::cout << "Hotel Name: " << current->data.name << std::endl;
+        current = current->next;
+    }
+    hotels.sort();
+
+    std::cout << "After sorting: " << std::endl;
+    current = hotels.head;
+    while (current != nullptr) {
+        std::cout << "Hotel Name: " << current->data.name << std::endl;
+        current = current->next;
+    }
+
     return 0;
 }
